6636d8e378fdd6477c3ce54e99f46ce2b7eb7a0a
willclarktech <willclarktech@users.noreply.github.com>
2020-11-30 14:41:26 +0000

    :house_with_garden: Update reqs script

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
 pipenv lock --requirements > requirements.txt
 pipenv lock --requirements --dev > requirements.dev.txt
 # These packages are preloaded on Colab and would require a runtime restart
