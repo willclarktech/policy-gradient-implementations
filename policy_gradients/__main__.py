2b1a046a3f1d77bf5e409fce35e93e5b16b16e9a
willclarktech <willclarktech@users.noreply.github.com>
2020-11-30 12:25:30 +0000

    :seedling: Handle errors in __main__

--- policy_gradients/__main__.py
+++ policy_gradients/__main__.py
@@ -1,6 +1,13 @@
+import sys
+
 from policy_gradients.runner import algorithms, run
 from policy_gradients.parser import create_parser
 
 parser = create_parser(algorithms.keys())
 args = parser.parse_args()
-run(vars(args))
+
+try:
+    run(vars(args))
+except Exception as e:
+    print(f"{type(e).__name__}: {e}")
+    sys.exit(1)
