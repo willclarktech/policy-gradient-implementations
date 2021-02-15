fcebdfd81c9606dec06e40c672561ba78295ee90
willclarktech <willclarktech@users.noreply.github.com>
2021-02-15 15:44:21 +0000

    :house_with_garden: Update generate_requirements.sh

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
-command -v shellcheck > /dev/null && shellcheck "$0"
+command -v shellcheck >/dev/null && shellcheck "$0"
 
-pipenv lock --requirements > requirements.txt
-pipenv lock --requirements --dev > requirements.dev.txt
+poetry export --without-hashes --output requirements.txt
+poetry export --dev --without-hashes --output requirements.dev.txt
 # These packages are preloaded on Colab and would require a runtime restart
-grep -ivE "^matplotlib==|^numpy==|^pillow==" requirements.txt > requirements.colab.txt
+grep -ivE "^matplotlib==|^numpy==|^pillow==" requirements.txt >requirements.colab.txt
